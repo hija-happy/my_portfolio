0b1f449b1c9caad005d61fafcd8d1b5af251bf71
hija-happy <hijahappy2004@gmail.com>
2024-09-09 11:40:13 +0530

    Home Page Updated

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import profilePic from '../assets/icons/profile.png';
 import './AboutSection.css'; // Import custom CSS for AboutSection
 
 const AboutSection = () => {
   return (
-    <section id="about" className="about-section fade-in">
-      <h2>About Me</h2>
-      <p className='fade-in'>Hi, I'm Hija Happy, a passionate developer and tech enthusiast. I enjoy working on web and mobile applications, and I have a keen interest in photography and portrait sketching. I'm currently working on projects related to emotion detection, image processing, and more. Let's connect and collaborate!</p>
+    <section id = 'about' className='about-section'> 
+      <div className='about-row'>
+
+        {/* TEXT */}
+        <div className='about-text'>
+          <p className='big-text'>I'm Hija Happy</p>
+          <h3 className='medium-text'>Engineering Student and Tech Enthusiast</h3>
+          <p className='small-text'>Hi, I'm Hija Happy, a passionate developer and tech enthusiast. I enjoy working on web and mobile applications, and I have a keen interest in photography and portrait sketching. I'm currently working on projects related to emotion detection, image processing, and more. Let's connect and collaborate!</p>
+        </div>
+
+        {/* PROFILE PIC */}
+        <div className='about-image'> 
+          <img src={profilePic} alt='Hija Happy' className='profile-pic'></img>
+        </div>
+
+      </div>
     </section>
   );
 };
